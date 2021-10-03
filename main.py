719450914ce4998a6e14da5736109a6c278e0f36
Tahlil <bsse0803@iit.du.ac.bd>
2021-10-03 19:33:20 +0600

    renove usd

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ ether = 1000000000000000000
 with open("./abis/IPriceOracleGetter.json") as f:
     IPrice_Oracle_Getter = json.load(f)  
 
-cg = CoinGeckoAPI()
 coin_dict = {
 "cusd": "cUSD",
 "celo": "Celo",
@@ -42,10 +41,7 @@ price_oracle = eth.contract(address='0x88A4a87eF224D8b1F463708D0CD62b17De593DAd'
 def get_price_in_celo(coin_name, coin_address):
     return (price_oracle.functions.getAssetPrice(coin_address).call()/ether)
 
-def get_exchange_rate_in_usd(coin_name, coin_address):
-    price_in_celo = get_price_in_celo(coin_name, coin_address)
-    return price_in_celo, price_in_celo*cg.get_price(ids='celo', vs_currencies='usd')['celo']['usd']
- 
+
 URL = "https://aj-v1-moola-downstream-api.herokuapp.com/"
 
 async def fetch(session, url, params):
@@ -76,30 +72,9 @@ def dump_data(api_url, params, method):
     print(URL+api_url)
     asyncio.run(fetch(URL+api_url, params, method))
 
-def dump_coin_exchange_rate(coinName, network, usd_exchange_rate, celo_exchange_rate, cusd_exchange_rate, ceuro_exchange_rate, block_number):
-    return URL+'set/insert/db_celo_testnet/tbl_coin_exchange_rate', {'coin_name': coin_dict[coinName], 'network_name': network, 'usd_exchange_rate': usd_exchange_rate, "celo_exchange_rate":celo_exchange_rate, "cusd_exchange_rate":cusd_exchange_rate, "ceuro_exchange_rate":ceuro_exchange_rate, 'agent_id':0, 'block_number': block_number, "block_number__Type": "int"}, 'GET'
+def dump_coin_exchange_rate(coinName, network, celo_exchange_rate, cusd_exchange_rate, ceuro_exchange_rate, block_number):
+    return URL+'set/insert/db_celo_testnet/tbl_coin_exchange_rate', {'coin_name': coin_dict[coinName], 'network_name': network, "celo_exchange_rate":celo_exchange_rate, "cusd_exchange_rate":cusd_exchange_rate, "ceuro_exchange_rate":ceuro_exchange_rate, 'agent_id':0, 'block_number': block_number, "block_number__Type": "int"}, 'GET'
 
-
-# @sched.scheduled_job('interval', minutes=5)
-# def timed_job():
-#     celo_mainnet_latest_block = get_latest_block(helper_w3)
-#     exchange_rate_reqs = []
-#     celo_usd, cusd_usd, ceuro_usd = get_exchange_rate_in_usd("celo", coins_reserve_address["celo"]), get_exchange_rate_in_usd("cusd", coins_reserve_address["cusd"]),get_exchange_rate_in_usd("ceuro", coins_reserve_address["ceuro"])
-#     cusd_in_celo, ceuro_in_celo = get_price_in_celo("cusd", coins_reserve_address["cusd"]), get_price_in_celo("ceuro", coins_reserve_address["ceuro"])
-
-#     url, params, method = dump_coin_exchange_rate('celo', 'celo mainnet',  celo_usd, 1, 1/cusd_in_celo, 1/ceuro_in_celo,celo_mainnet_latest_block)
-#     exchange_rate_reqs.append({"url": url, "params": params, "method": method})
-        
-#     url, params, method = dump_coin_exchange_rate('cusd', 'celo mainnet',  cusd_usd, cusd_in_celo, 1, cusd_in_celo/ceuro_in_celo,celo_mainnet_latest_block)
-#     exchange_rate_reqs.append({"url": url, "params": params, "method": method})
-
-#     url, params, method = dump_coin_exchange_rate('ceuro', 'celo mainnet',  ceuro_usd, ceuro_in_celo, ceuro_in_celo/cusd_in_celo, 1,celo_mainnet_latest_block)
-#     exchange_rate_reqs.append({"url": url, "params": params, "method": method})
-
-   
-#     asyncio.run(fetch_all(exchange_rate_reqs))
-
-# sched.start()
 
 
 
@@ -107,16 +82,16 @@ def dump_exhcange():
     print("Dumping exchange data...")
     celo_mainnet_latest_block = get_latest_block(helper_w3)
     exchange_rate_reqs = []
-    celo_usd, cusd_usd, ceuro_usd = get_exchange_rate_in_usd("celo", coins_reserve_address["celo"]), get_exchange_rate_in_usd("cusd", coins_reserve_address["cusd"]),get_exchange_rate_in_usd("ceuro", coins_reserve_address["ceuro"])
+    # celo_usd, cusd_usd, ceuro_usd = get_exchange_rate_in_usd("celo", coins_reserve_address["celo"]), get_exchange_rate_in_usd("cusd", coins_reserve_address["cusd"]),get_exchange_rate_in_usd("ceuro", coins_reserve_address["ceuro"])
     cusd_in_celo, ceuro_in_celo = get_price_in_celo("cusd", coins_reserve_address["cusd"]), get_price_in_celo("ceuro", coins_reserve_address["ceuro"])
 
-    url, params, method = dump_coin_exchange_rate('celo', 'celo mainnet',  celo_usd, 1, 1/cusd_in_celo, 1/ceuro_in_celo,celo_mainnet_latest_block)
+    url, params, method = dump_coin_exchange_rate('celo', 'celo mainnet', 1, 1/cusd_in_celo, 1/ceuro_in_celo, celo_mainnet_latest_block)
     exchange_rate_reqs.append({"url": url, "params": params, "method": method})
         
-    url, params, method = dump_coin_exchange_rate('cusd', 'celo mainnet',  cusd_usd, cusd_in_celo, 1, cusd_in_celo/ceuro_in_celo,celo_mainnet_latest_block)
+    url, params, method = dump_coin_exchange_rate('cusd', 'celo mainnet', cusd_in_celo, 1, cusd_in_celo/ceuro_in_celo,celo_mainnet_latest_block)
     exchange_rate_reqs.append({"url": url, "params": params, "method": method})
 
-    url, params, method = dump_coin_exchange_rate('ceuro', 'celo mainnet',  ceuro_usd, ceuro_in_celo, ceuro_in_celo/cusd_in_celo, 1,celo_mainnet_latest_block)
+    url, params, method = dump_coin_exchange_rate('ceuro', 'celo mainnet', ceuro_in_celo, ceuro_in_celo/cusd_in_celo, 1,celo_mainnet_latest_block)
     exchange_rate_reqs.append({"url": url, "params": params, "method": method})
    
     asyncio.run(fetch_all(exchange_rate_reqs))
